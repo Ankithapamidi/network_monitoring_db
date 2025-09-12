714d372501fa306b10a1b892272ebf07ffa3e9ed
Ankithapamidi <paamidiankitha@gmail.com>
2025-09-12 23:29:09 +0530

    Add files via upload

---
+++ employees_rows.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."employees" ("id", "name", "role", "salary") VALUES ('1', 'Ankitha', 'Database Manager', '60000'), ('2', 'Harish', 'Backend Developer', '55000'), ('3', 'Sita', 'Frontend Developer', '50000'), ('4', 'Ravi', 'Tester', '45000');
